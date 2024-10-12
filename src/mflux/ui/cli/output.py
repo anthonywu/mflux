12adb04a5d401ff8c9c94673469c826efbcf0604
Anthony Wu <462072+anthonywu@users.noreply.github.com>
2024-10-12 02:14:36 -0700

    bring in console.print

--- src/mflux/ui/cli/output.py
+++ src/mflux/ui/cli/output.py
@@ -1,14 +1,26 @@
 try:
     # use the fancy progress bar if 'rich' is installed
     # https://tqdm.github.io/docs/rich/
-    from tqdm.rich import tqdm as progress
-    from tqdm import TqdmExperimentalWarning
-
     import warnings
+    from functools import partial
+
+    from rich.console import Console
+    from tqdm import TqdmExperimentalWarning
+    from tqdm.rich import tqdm
 
     warnings.filterwarnings("ignore", category=TqdmExperimentalWarning)
-except ModuleNotFoundError as e:
+    # see help(Console), used to print messages to user
+    # https://rich.readthedocs.io/en/stable/console.html#printing
+    console = Console(color_system="auto")
+    progress = partial(
+        tqdm,
+        # options: see help(rich.progress.Progress)
+        options=dict(console=console, transient=True),
+    )
+except ModuleNotFoundError:
     # gracefully use standard progress bar
+    from unittest.mock import Mock
     from tqdm import tqdm as progress
+    progress.console = Mock(print=print)
 
 __all__ = ["progress"]

7835716fb691e693f0799a5c4c5775c36d252377
Anthony Wu <462072+anthonywu@users.noreply.github.com>
2024-10-12 01:53:09 -0700

    suppress TqdmExperimentalWarning

--- src/mflux/ui/cli/output.py
+++ src/mflux/ui/cli/output.py
@@ -2,10 +2,13 @@ try:
     # use the fancy progress bar if 'rich' is installed
     # https://tqdm.github.io/docs/rich/
     from tqdm.rich import tqdm as progress
-except ModuleNotFoundError:
+    from tqdm import TqdmExperimentalWarning
+
+    import warnings
+
+    warnings.filterwarnings("ignore", category=TqdmExperimentalWarning)
+except ModuleNotFoundError as e:
     # gracefully use standard progress bar
     from tqdm import tqdm as progress
 
-__all__ = [
-    "progress"
-]
+__all__ = ["progress"]
